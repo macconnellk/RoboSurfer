c10e2155e53b17307fbcb6f4e997c2fcaed34941
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-01-15 00:01:44 -0500

    Update NightBoost.js
    Build Code

--- NightBoost.js
+++ NightBoost.js
@@ -26,15 +26,16 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
    var NightBoostISF_ROC = .5 // High ROC Nightboost ISF % Increase    
    var NightBoostSMBUAMMinutes = 15; // Standard Nightboost SMB/UAM Increase
    var NightBoostSMBUAMMinutes_ROC = 30; // High ROC Nightboost SMB/UAM Increase
-       
-       
+   var NightBoostSMBDeliveryRatio = 1; // Nightboost SMB Delivery Ratio   
+             
 //  Initialize function variables
    var myGlucose = glucose[0].glucose;
    var target = profile.min_bg;
    var isf = profile.sens;
    var COB = meal.carbs;    
    var maxSMB = profile.maxSMBBasalMinutes
-   var maxUAM = profile.maxUAMSMBBasalMinutes     
+   var maxUAM = profile.maxUAMSMBBasalMinutes  
+   var SMBDeliveryRatio = profile.smb_delivery_ratio;    
    const now = new Date();
    const NightBoostStart = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 0, 0); // 8:00 PM
    var ROC = 0;    
@@ -42,12 +43,16 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
   if (now > NightBoostStart && COB > NightBoostCarbThreshold && myGlucose > NightBoostBGThreshold) {
    
       profile.sens = 
-
-
-     
+      profile.maxSMBBasalMinutes =    
+      profile.maxUAMSMBBasalMinutes =    
+      profile.smb_delivery_ratio = 
+         
+         
      if (ROC >= NightBoostROCThreshold) {
+         profile.sens = 
          profile.maxSMBBasalMinutes = maxSMB + NightBoostSMBUAMMinutesROC
          profile.maxUAMSMBBasalMinutes = maxUAM + + NightBoostSMBUAMMinutesROC
+         profile.smb_delivery_ratio = 
         }
     
   }
