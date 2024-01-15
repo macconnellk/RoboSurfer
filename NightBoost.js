a65e1d627f20611615c0a44ea4ffafa532f0cb87
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-01-15 09:12:32 -0500

    Update NightBoost.js

--- NightBoost.js
+++ NightBoost.js
@@ -1,3 +1,7 @@
+
+
+// WORK IN PROGRESS
+
 function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory, preferences, basal_profile, oref2_variables) {
 
    function round(value, digits) {
