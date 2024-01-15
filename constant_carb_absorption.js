09e53f04b5446e0a83b3117cde349672f3a56511
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-01-15 09:29:12 -0500

    Update constant_carb_absorption.js
    Updated variable names

--- constant_carb_absorption.js
+++ constant_carb_absorption.js
@@ -10,17 +10,18 @@ function round(value, digits) {
 
 //  User-defined function variables        
 // Define the minimum amount of carb you wamt iAPS to decay in 1 hour.
-var min_carb_absorption = 24;
+var min_hourly_carb_absorption = 24;
         
 //  Initialize function variables
   const isf = profile.sens;
   const carb_factor = profile.carb_ratio;
+  var min_5m_carbabsorption = 0;
   var min_5m_carbimpact = 0;
 
 // The Constant Carb Absorption Function
 
   // Reduce hourly carb absorption to 5-minute carb absoorption
-     min_5m_carbabsorption = min_carb_absorption / (60 / 5);
+     min_5m_carbabsorption = min_hourly_carb_absorption / (60 / 5);
 
   // Calculate the dynamic min_5m_carbimpact
    min_5m_carbimpact = (min_5m_carbabsorption * isf) / carb_factor;
