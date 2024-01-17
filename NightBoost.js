bd568756cfa6017830a63d0559860859ea8e6903
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-01-16 20:26:41 -0500

    Update NightBoost.js
    Update variable names for use in RoboSurfer

--- NightBoost.js
+++ NightBoost.js
@@ -24,49 +24,48 @@ if (enable_nightboost) {
       var NightBoost_ROCThreshold = 0; // TBD  
 
    // User-defined Settings Increases     
-      var ISF_CR_NightBoost = .25; // Standard Nightboost ISF % Increase
-      var ISF_CR_ROC_NightBoost = .5; // High ROC Nightboost ISF % Increase    
-      var SMBUAMMinutes_NightBoost = 15; // Standard Nightboost SMB/UAM Increase
-      var SMBUAMMinutes_ROC_NightBoost = 30; // High ROC Nightboost SMB/UAM Increase
-      var SMBDeliveryRatio_NightBoost = 1; // Nightboost SMB Delivery Ratio  
-      var COB_Max_Nightboost = 100; // Nightboost COB_Max
+      var ISF_CR_NightBoostIncrease = .25; // Standard Nightboost ISF % Increase
+      var ISF_CR_ROC_NightBoostIncrease = .5; // High ROC Nightboost ISF % Increase    
+      var SMBUAMMinutes_NightBoostIncrease = 15; // Standard Nightboost SMB/UAM Increase
+      var SMBUAMMinutes_ROC_NightBoostIncrease = 30; // High ROC Nightboost SMB/UAM Increase
+      var SMBDeliveryRatio_NightBoostIncrease = 1; // Nightboost SMB Delivery Ratio  
+      var COB_Max_NightboostIncrease = 100; // Nightboost COB_Max
            
    //  Initialize function variables
       var NightBoost_Status = "Off";
       var myGlucose = glucose[0].glucose;
       var target = profile.min_bg;
-      var isf = profile.sens;
-      var cr = profile.carb_ratio;
-      var csf = isf/cr; 
+      var isf_NightBoostStart = profile.sens;
+      var cr_NightboostStart = profile.carb_ratio;
+      var csf_NightboostStart = isf_NightBoostStart / cr_NightboostStart; 
       var max_COB = profile.maxCOB;   
       var maxSMB = profile.maxSMBBasalMinutes;
       var maxUAM = profile.maxUAMSMBBasalMinutes;  
-      var SMBDeliveryRatio = profile.smb_delivery_ratio; 
+      var SMBDeliveryRatio_NightBoostStart = profile.smb_delivery_ratio; 
       var COB = meal.mealCOB; 
       const now = new Date();
       const NightBoostStart = new Date(now.getFullYear(), now.getMonth(), now.getDate(), NightBoost_StartTimeHour, NightBoost_StartTimeMinute, 0); 
       var ROC = 0;  
       var NightBoosted_isf = 0;
-      var NightBooster_cr = 0;
+      var NightBoosted_cr = 0;
       var check_csf = 0;
 
       //Add BG Rate of Change Function
-
 
       if (now >= NightBoostStart && 
           myGlucose > NightBoost_BGThreshold &&
           COB > NightBoost_CarbThreshold) {
             
             var NightBoost_Status = "On";
-            var NightBoosted_isf = isf - (isf * ISF_CR_NightBoost);
-            var NightBooster_cr = NightBoosted_isf / csf;
+            var NightBoosted_isf = isf_NightBoostStart - (isf_NightBoostStart * ISF_CR_NightBoostIncrease);
+            var NightBoosted_cr = NightBoosted_isf / csf_NightboostStart;
             profile.sens = NightBoosted_isf;
-            profile.carb_ratio = NightBooster_cr;  
+            profile.carb_ratio = NightBoosted_cr;  
             var check_csf = profile.sens / profile.carb_ratio;
-            profile.maxSMBBasalMinutes = maxSMB + SMBUAMMinutes_NightBoost;   
-            profile.maxUAMSMBBasalMinutes = maxUAM + SMBUAMMinutes_NightBoost;   
-            profile.smb_delivery_ratio = SMBDeliveryRatio_NightBoost;
-            profile.maxCOB = COB_Max_Nightboost; 
+            profile.maxSMBBasalMinutes = maxSMB + SMBUAMMinutes_NightBoostIncrease;   
+            profile.maxUAMSMBBasalMinutes = maxUAM + SMBUAMMinutes_NightBoostIncrease;   
+            profile.smb_delivery_ratio = SMBDeliveryRatio_NightBoostIncrease;
+            profile.maxCOB = COB_Max_NightboostIncrease; 
             var min_carb_absorption = 12; // Option to change carb absorption e.g. slower after bedtime after late meals. Assumes use of constant_carb_absorption function
             
           //   if (ROC >= NightBoostROCThreshold) {
