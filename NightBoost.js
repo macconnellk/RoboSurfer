5378591b974543a5ca7ac0d22b5052d8ae232f6d
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-01-17 15:42:27 -0500

    Update NightBoost.js
    Updated code to use boosted CSF

--- NightBoost.js
+++ NightBoost.js
@@ -17,15 +17,17 @@ if (enable_nightboost) {
 
    // Initialize Function Variables
    // Thresholds
-      var NightBoost_StartTimeHour = 17; // 8pm
+      var NightBoost_StartTimeHour = 12; // 8pm
       var NightBoost_StartTimeMinute = 0; // 8:00pm
       var NightBoost_CarbThreshold = 40; // COB
       var NightBoost_BGThreshold = 140; // BG over
       var NightBoost_ROCThreshold = 0; // TBD  
 
-   // User-defined Settings Increases     
-      var ISF_CR_NightBoostIncrease = .25; // Standard Nightboost ISF % Increase
-      var ISF_CR_ROC_NightBoostIncrease = .5; // High ROC Nightboost ISF % Increase    
+   // User-defined Settings Increases 
+   // Note: To reflect slower digestion and increased impact of carbs, CSF must increase
+   // To do so while ISF strenghtens (decreases), CR must strenghten (decrease) more than ISF
+      var CSF_NightboostStrengthFactor = 1.5; // Used to calculate new CR
+      var ISF_NightBoostStrengthFactor = .25; // Standard Nightboost ISF % Strenghten
       var SMBUAMMinutes_NightBoostIncrease = 15; // Standard Nightboost SMB/UAM Increase
       var SMBUAMMinutes_ROC_NightBoostIncrease = 30; // High ROC Nightboost SMB/UAM Increase
       var SMBDeliveryRatio_NightBoostIncrease = 1; // Nightboost SMB Delivery Ratio  
@@ -44,8 +46,8 @@ if (enable_nightboost) {
       var SMBDeliveryRatio_NightBoostStart = profile.smb_delivery_ratio; 
       var COB = meal.mealCOB; 
       const now = new Date();
-      const NightBoostStart = new Date(now.getFullYear(), now.getMonth(), now.getDate(), NightBoost_StartTimeHour, NightBoost_StartTimeMinute, 0); 
-      var ROC = 0;  
+      const NightBoostStart = new Date(now.getFullYear(), now.getMonth(), now.getDate(), NightBoost_StartTimeHour, NightBoost_StartTimeMinute, 0);   
+      var NightBoosted_csf = 0;
       var NightBoosted_isf = 0;
       var NightBoosted_cr = 0;
       var check_csf = 0;
@@ -56,17 +58,18 @@ if (enable_nightboost) {
           myGlucose > NightBoost_BGThreshold &&
           COB > NightBoost_CarbThreshold) {
             
-            var NightBoost_Status = "On";
-            var NightBoosted_isf = isf_NightBoostStart - (isf_NightBoostStart * ISF_CR_NightBoostIncrease);
-            var NightBoosted_cr = NightBoosted_isf / csf_NightboostStart;
+            NightBoost_Status = "On";
+            NightBoosted_csf = csf_NightboostStart * CSF_NightboostStrengthFactor
+            NightBoosted_isf = isf_NightBoostStart - (isf_NightBoostStart * ISF_NightBoostStrengthFactor);
+            NightBoosted_cr = NightBoosted_isf /  NightBoosted_csf;
             profile.sens = NightBoosted_isf;
             profile.carb_ratio = NightBoosted_cr;  
-            var check_csf = profile.sens / profile.carb_ratio;
+            check_csf = profile.sens / profile.carb_ratio;
             profile.maxSMBBasalMinutes = maxSMB + SMBUAMMinutes_NightBoostIncrease;   
             profile.maxUAMSMBBasalMinutes = maxUAM + SMBUAMMinutes_NightBoostIncrease;   
             profile.smb_delivery_ratio = SMBDeliveryRatio_NightBoostIncrease;
             profile.maxCOB = COB_Max_NightboostIncrease; 
-            var min_carb_absorption = 12; // Option to change carb absorption e.g. slower after bedtime after late meals. Assumes use of constant_carb_absorption function
+            var min_carb_absorption = 11; // Option to change carb absorption e.g. slower after bedtime after late meals. Assumes use of constant_carb_absorption function
             
           //   if (ROC >= NightBoostROCThreshold) {
           //      profile.sens = 
@@ -76,7 +79,7 @@ if (enable_nightboost) {
        
         }
    
-return "Nightboost Status: :" + NightBoost_Status + "Nightboost Start Time: " + NightBoostStart + " ISF: "  + round(profile.sens, 2) + " CR: "  + round(profile.carb_ratio, 2) + " CSF Check: Profile: "  + round(csf_NightboostStart, 2) + " New: " + round(check_csf, 2) + " SMB Minutes: "  + round(profile.maxSMBBasalMinutes, 2) + " UAM Minutes: "  + round(profile.maxUAMSMBBasalMinutes, 2) + " SMB Delivery Ratio: "  + round(profile.smb_delivery_ratio, 2) + " Max COB: "  + round(profile.maxCOB, 2) + " Min Absorption: "  + round(min_carb_absorption, 2);
+return "Nightboost Status: " + NightBoost_Status + " Nightboost Start Time: " + NightBoostStart + " NightBoost ISF: "  + round(profile.sens, 2) + " Nightboost CR: "  + round(profile.carb_ratio, 2) + " CSF Check: Profile CSF: "  + round(csf_NightboostStart, 2) + " Nightboost CSF: " + round(check_csf, 2) + " SMB Minutes: "  + round(profile.maxSMBBasalMinutes, 2) + " UAM Minutes: "  + round(profile.maxUAMSMBBasalMinutes, 2) + " SMB Delivery Ratio: "  + round(profile.smb_delivery_ratio, 2) + " Max COB: "  + round(profile.maxCOB, 2) + " Min Absorption: "  + round(min_carb_absorption, 2);
 }       
 
   
