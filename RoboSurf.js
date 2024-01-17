85f8c6c2225e3ea7046727f19f1f1039e1e839b7
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-01-17 16:03:50 -0500

    Update RoboSurf.js
    Revert code

--- RoboSurf.js
+++ RoboSurf.js
@@ -15,6 +15,10 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
 //Turn RoboSurfer on or off
   var enable_sigmoidTDD = true;
 
+// DYNAMIC ISF: SIGMOID WITH ENHANCED TDD RESPONSE
+//Turn on or off
+  var enable_sigmoidTDD = true;
+
 //Only use when enable_sigmoidTDD = true.
 // Dynamic ISF and Sigmoid must be on in settings with Min and Max both set to 1 (necessary for past2hraverage to calculate
     if (enable_sigmoidTDD) { 
@@ -24,47 +28,15 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
    
 //  Initialize RoboSurfer function variables
    var myGlucose = glucose[0].glucose;
+   var minimumRatio = .99;
+   var maximumRatio = 1.25;
+   var adjustmentFactor = .65;
    var target = profile.min_bg;
    var past2hoursAverage = oref2_variables.past2hoursAverage;
    var average_total_data = oref2_variables.average_total_data;
    var weightedAverage = oref2_variables.weightedAverage;
    var isf = profile.sens;
 
-      // Enhanced Sigmoid Variables 
-       var minimumRatio = .99;
-      var maximumRatio = 1.25;
-      var adjustmentFactor = .65;
-       
-      // NightBoost Variables
-      var NightBoost_Status = "Off";
-      var myGlucose = glucose[0].glucose;
-      var target = profile.min_bg;
-      var isf_NightBoostStart = profile.sens;
-      var cr_NightboostStart = profile.carb_ratio;
-      var csf_NightboostStart = isf_NightBoostStart / cr_NightboostStart; 
-      var max_COB = profile.maxCOB;   
-      var maxSMB = profile.maxSMBBasalMinutes;
-      var maxUAM = profile.maxUAMSMBBasalMinutes;  
-      var SMBDeliveryRatio_NightBoostStart = profile.smb_delivery_ratio; 
-      var COB = meal.mealCOB; 
-      const now = new Date();
-      const NightBoostStart = new Date(now.getFullYear(), now.getMonth(), now.getDate(), NightBoost_StartTimeHour, NightBoost_StartTimeMinute, 0);   
-      var NightBoosted_csf = 0;
-      var NightBoosted_isf = 0;
-      var NightBoosted_cr = 0;
-      var check_csf = 0;
-       
-      // Minimum Carb Absorption Variables
-      // Define the minimum amount of carb you wamt iAPS to decay in 1 hour.
-      var min_hourly_carb_absorption = 24;
-
-// TDD-Factor Sigmoid Function
-     
-// DYNISF SIGMOID MODIFICATION #1
-// Define a TDD Factor using a Sigmoid curve that approximates the TDD delta effect used in the Chris Wilson DynISF approach.
-// This TDD delta effect is not linear across BGs and requires a curve to mimic.
-// ORIGINAL SIGMOID APPROACH: const tdd_factor = tdd_averages.weightedAverage / tdd_averages.average_total_data;
-       
 // Sensitivity Protection Mechanism: If 24hr TDD is less than 2-Week TDD (more sensitive), set weighted average TDD to the 24hr TDD value)
    if (past2hoursAverage < average_total_data) {
       weightedAverage = past2hoursAverage;
@@ -77,6 +49,13 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
          var log_protectionmechanism = "OnZero";
       }
          
+// TDD-Factor Sigmoid Function
+     
+// DYNISF SIGMOID MODIFICATION #1
+// Define a TDD Factor using a Sigmoid curve that approximates the TDD delta effect used in the Chris Wilson DynISF approach.
+// This TDD delta effect is not linear across BGs and requires a curve to mimic.
+// ORIGINAL SIGMOID APPROACH: const tdd_factor = tdd_averages.weightedAverage / tdd_averages.average_total_data;
+
     // Define TDD deviation variable for use in TDD Sigmoid curve based on current percent change between Daily TDD deviation and 2 Week Deviation 
     // This approach will normalize this variable for any TDD value to ensure a standard TDD Factor sigmoid curve for all users
    var tdd_dev = (weightedAverage / average_total_data - 1) * 10;
@@ -153,82 +132,29 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
    // Set profile to new value
      profile.smb_delivery_ratio = round(smb_delivery_ratio,2);
 
-// ROBOSURFER ENHANCEMENT #3: NIGHTBOOST       
-// NIGHTBOOST FUNCTION
-//Turn on or off
-  var enable_nightboost = true;
-
-//Only use when enable_robotune = true
-if (enable_nightboost) { 
-
-   // Initialize Function Variables
-   // Thresholds
-      var NightBoost_StartTimeHour = 12; // 8pm
-      var NightBoost_StartTimeMinute = 0; // 8:00pm
-      var NightBoost_CarbThreshold = 40; // COB
-      var NightBoost_BGThreshold = 140; // BG over
-      var NightBoost_ROCThreshold = 0; // TBD  
-
-   // User-defined Settings Increases 
-   // Note: To reflect slower digestion and increased impact of carbs, CSF must increase
-   // To do so while ISF strenghtens (decreases), CR must strenghten (decrease) more than ISF
-      var CSF_NightboostStrengthFactor = 1.5; // Used to calculate new CR
-      var ISF_NightBoostStrengthFactor = .25; // Standard Nightboost ISF % Strenghten
-      var SMBUAMMinutes_NightBoostIncrease = 15; // Standard Nightboost SMB/UAM Increase
-      var SMBUAMMinutes_ROC_NightBoostIncrease = 30; // High ROC Nightboost SMB/UAM Increase
-      var SMBDeliveryRatio_NightBoostIncrease = 1; // Nightboost SMB Delivery Ratio  
-      var COB_Max_NightboostIncrease = 100; // Nightboost COB_Max
-
-      //Add BG Rate of Change Function
-
-      if (now >= NightBoostStart && 
-          myGlucose > NightBoost_BGThreshold &&
-          COB > NightBoost_CarbThreshold) {
-            
-            NightBoost_Status = "On";
-            NightBoosted_csf = csf_NightboostStart * CSF_NightboostStrengthFactor
-            NightBoosted_isf = isf_NightBoostStart - (isf_NightBoostStart * ISF_NightBoostStrengthFactor);
-            NightBoosted_cr = NightBoosted_isf /  NightBoosted_csf;
-            profile.sens = NightBoosted_isf;
-            profile.carb_ratio = NightBoosted_cr;  
-            check_csf = profile.sens / profile.carb_ratio;
-            profile.maxSMBBasalMinutes = maxSMB + SMBUAMMinutes_NightBoostIncrease;   
-            profile.maxUAMSMBBasalMinutes = maxUAM + SMBUAMMinutes_NightBoostIncrease;   
-            profile.smb_delivery_ratio = SMBDeliveryRatio_NightBoostIncrease;
-            profile.maxCOB = COB_Max_NightboostIncrease; 
-            var min_carb_absorption = 11; // Option to change carb absorption e.g. slower after bedtime after late meals. Assumes use of constant_carb_absorption function
-            
-          //   if (ROC >= NightBoostROCThreshold) {
-          //      profile.sens = 
-          //      profile.maxSMBBasalMinutes = maxSMB + NightBoostSMBUAMMinutesROC
-          //     profile.maxUAMSMBBasalMinutes = maxUAM + + NightBoostSMBUAMMinutesROC 
-          //    }
-       
-        }
-
-// ROBOSURFER ENHANCEMENT #4: SET CONSTANT MINIMUM HOURLY CARB ABSORPTION
+// ROBOSURFER ENHANCEMENT #3: SET CONSTANT MINIMUM HOURLY CARB ABSORPTION
 // For this function, the user should enter desired MIN CARB ABSORPTION in the min_5m_carbimpact setting instead of a min_5m_carbimpact.
 // The function will define the min_5m_carbimpact needed for that MIN CARB ABSORPTION based on current ISF and CR. 
        
-         
 //  Initialize function variables
-  const MinAbsorption_isf = profile.sens;
-  const MinAbsorption_carb_factor = profile.carb_ratio;
-  var min_5m_carbabsorption = 0;
+  var carb_ratio = profile.carb_ratio;
+  var min_carb_absorption = profile.min_5m_carbimpact;
+   var min_5m_carbabsorption = 0;
   var min_5m_carbimpact = 0;
 
 // The Constant Carb Absorption Function
 
   // Reduce hourly carb absorption to 5-minute carb absoorption
-     min_5m_carbabsorption = min_hourly_carb_absorption / (60 / 5);
+     min_5m_carbabsorption = min_carb_absorption / (60 / 5);
 
   // Calculate the dynamic min_5m_carbimpact
-   min_5m_carbimpact = (min_5m_carbabsorption * MinAbsorption_isf) / MinAbsorption_carb_factor;
-  
+   min_5m_carbimpact = (min_5m_carbabsorption * new_isf) / carb_ratio;
+
+   //Set profile to new value
   profile.min_5m_carbimpact = round(min_5m_carbimpact,2);
 
 // End RoboSurfer Enhancements
                         
- return "Autosens ratio set to: " + round(autosens.ratio, 2) + ". Sens Protect is " + log_protectionmechanism + ". ISF set from: " + round(isf, 2) + " to " + profile.sens + " TDD:" + round(past2hoursAverage, 2) + " Two-week TDD:" + round(average_total_data, 2) + " Weighted Average:" + round(weightedAverage, 2) + ". SMB Delivery Ratio: " + profile.smb_delivery_ratio + ". Min_5m_carbimpact: " +  profile.min_5m_carbimpact + "Nightboost Status: " + NightBoost_Status + " Nightboost Start Time: " + NightBoostStart + " NightBoost ISF: "  + round(profile.sens, 2) + " Nightboost CR: "  + round(profile.carb_ratio, 2) + " CSF Check: Profile CSF: "  + round(csf_NightboostStart, 2) + " Nightboost CSF: " + round(check_csf, 2) + " SMB Minutes: "  + round(profile.maxSMBBasalMinutes, 2) + " UAM Minutes: "  + round(profile.maxUAMSMBBasalMinutes, 2) + " SMB Delivery Ratio: "  + round(profile.smb_delivery_ratio, 2) + " Max COB: "  + round(profile.maxCOB, 2) + " Min Absorption: "  + round(min_carb_absorption, 2);
+ return "Autosens ratio set to: " + round(autosens.ratio, 2) + ". Sens Protect is " + log_protectionmechanism + ". ISF set from: " + round(isf, 2) + " to " + profile.sens + " TDD:" + round(past2hoursAverage, 2) + " Two-week TDD:" + round(average_total_data, 2) + " Weighted Average:" + round(weightedAverage, 2) + ". SMB Delivery Ratio: " + profile.smb_delivery_ratio + ". Min_5m_carbimpact: " +  profile.min_5m_carbimpact;
    }
 }
