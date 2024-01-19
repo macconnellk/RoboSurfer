51767ad949683baa706a1dafb92b7071a5dc163b
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-01-18 23:03:30 -0500

    Update GlucoseRateOfChange.js
    Build Code

--- GlucoseRateOfChange.js
+++ GlucoseRateOfChange.js
@@ -18,16 +18,15 @@ function round(value, digits) {
             
   const target = profile.min_bg;
   var smb_delivery_ratio = profile.smb_delivery_ratio;
-  
- const dateString0 = "2024-01-18T23:07:34.412Z";
-const dateString1 = "2024-01-19T01:30:45.789Z";
 
 // Convert strings to Date objects
-const date0 = new Date(dateString0);
-const date1 = new Date(dateString1);
+const date0 = new Date(myGlucoseTime0);
+const date1 = new Date(myGlucoseTime1);
+const date2 = new Date(myGlucoseTime2);
+const date3 = new Date(myGlucoseTime3);
 
 // Calculate time difference in milliseconds
-const timeDifferenceMillis = date1 - date0;
+const timeDifferenceMillis = date2 - date0;
 
 // Convert milliseconds to days, hours, minutes, seconds
 const days = Math.floor(timeDifferenceMillis / (1000 * 60 * 60 * 24));
@@ -35,8 +34,7 @@ const hours = Math.floor((timeDifferenceMillis % (1000 * 60 * 60 * 24)) / (1000 
 const minutes = Math.floor((timeDifferenceMillis % (1000 * 60 * 60)) / (1000 * 60));
 const seconds = Math.floor((timeDifferenceMillis % (1000 * 60)) / 1000);
 
-console.log(`Time difference: ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
-return "Using Middleware function the glucose rate of change is: " + myGlucose0 + myGlucose1 + myGlucose2 + myGlucose3 + myGlucoseTime0 + myGlucoseTime1 + myGlucoseTime2 + myGlucoseTime3 + round(profile.smb_delivery_ratio, 2) + ".";
+return "Using Middleware function the glucose rate of change is: " + myGlucose0 + " " + myGlucose1 + " " + myGlucose2 + " " + myGlucose3 + " " + myGlucoseTime0 + " " + myGlucoseTime1 + " " + myGlucoseTime2 + " " + myGlucoseTime3 + " " + minutes + " " + secomds;
 
 } 
   
