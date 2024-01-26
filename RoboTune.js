18c553c2f9032b9c37fbb84b1bbd23edcef8f26e
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-01-25 19:22:34 -0500

    Update RoboTune.js
    Add data interpolation

--- RoboTune.js
+++ RoboTune.js
@@ -26,46 +26,60 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
     myGlucoseTime.push(new Date(element.dateString)); // Parse datestring to date object
       });      
        
-   var test1 = myGlucose[50];
-   var test2 = myGlucoseTime[50];
-   var test3 = myGlucoseTime[60];    
-   var testcalc = myGlucoseTime[2] - myGlucoseTime[0];
-   return test1 + " " + test2 + " " + test3 + testcalc;
        
 // Filter the data based on time ranges
-const getCurrentTime = () => new Date().getTime();
-
-const filterByTimeRange = (timeRange) => {
-    const currentTime = getCurrentTime();
+// Filter the data based on time ranges and interpolate any gaps greater than 5 minutes
+const filterByTimeRange = (timeRange, glucoseData) => {
+    const currentTime = new Date().getTime();
     const timeThreshold = currentTime - (timeRange * 60 * 60 * 1000);
 
     const filteredData = [];
 
-    for (let i = 0; i < dateArray.length; i++) {
-        const date = new Date(dateArray[i]).getTime();
+    for (let i = 0; i < glucoseData.length; i++) {
+        const date = new Date(glucoseData[i].datestring).getTime();
         if (date >= timeThreshold) {
-            filteredData.push({
-                glucose: glucoseArray[i],
-                datestring: dateArray[i]
-            });
+            filteredData.push(glucoseData[i]);
+        }
+    }
+
+    // Interpolate gaps greater than 5 minutes
+    for (let i = 1; i < filteredData.length; i++) {
+        const currentTime = new Date(filteredData[i].datestring).getTime();
+        const prevTime = new Date(filteredData[i - 1].datestring).getTime();
+        const timeDiff = (currentTime - prevTime) / (1000 * 60); // Difference in minutes
+
+        if (timeDiff > 5) {
+            const numInterpolatedPoints = Math.floor(timeDiff / 5) - 1; // Number of points to interpolate
+            const glucoseDiff = (filteredData[i].glucose - filteredData[i - 1].glucose) / (numInterpolatedPoints + 1);
+
+            for (let j = 1; j <= numInterpolatedPoints; j++) {
+                const interpolatedTime = new Date(prevTime + j * 5 * 60 * 1000).toISOString(); // Add 5 minutes
+                const interpolatedGlucose = filteredData[i - 1].glucose + glucoseDiff * j;
+                filteredData.splice(i + (j - 1), 0, { glucose: interpolatedGlucose, datestring: interpolatedTime });
+            }
         }
     }
 
     return filteredData;
 };
 
-// Separate the data into time ranges (last 4 hours, 8 hours, 12 hours, 16 hours, 24 hours)
-const last4HoursData = filterByTimeRange(4);
-const last8HoursData = filterByTimeRange(8);
-const last12HoursData = filterByTimeRange(12);
-const last16HoursData = filterByTimeRange(16);
-const last20HoursData = filterByTimeRange(20);       
-const last24HoursData = filterByTimeRange(24);
+// Separate the data into time ranges (last 4 hours, 8 hours, 12 hours, 16 hours, 20 hours, 24 hours)
+const last4HoursData = filterByTimeRange(4, glucose);
+const last8HoursData = filterByTimeRange(8, glucose);
+const last12HoursData = filterByTimeRange(12, glucose);
+const last16HoursData = filterByTimeRange(16, glucose);
+const last20HoursData = filterByTimeRange(20, glucose);
+const last24HoursData = filterByTimeRange(24, glucose);
 
-
-return last4HoursData + last8HoursData + last12HoursData+ last16HoursData + last20HoursData + last24HoursData;
-
-       
+// Return filtered and interpolated data for different time ranges
+      return {
+          last4HoursData,
+          last8HoursData,
+          last12HoursData,
+          last16HoursData,
+          last20HoursData,
+          last24HoursData
+      };
 }
-}
+}   
     
