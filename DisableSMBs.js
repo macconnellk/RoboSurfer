49bc2d5e2bd6c2ae401106eba15e37833db78ed5
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-02-03 13:11:30 -0500

    Update DisableSMBs.js
    Build code

--- DisableSMBs.js
+++ DisableSMBs.js
@@ -1,13 +1,25 @@
-const currentTime = new Date(filteredData[i].datestring).getTime();
-           const prevTime = new Date(filteredData[i - 1].datestring).getTime();
-           const timeDiff = (currentTime - prevTime) / (1000 * 60); // Difference in minutes
 
-           if (timeDiff > 5) {
-               const numInterpolatedPoints = Math.floor(timeDiff / 5) - 1; // Number of points to interpolate
-               const glucoseDiff = (filteredData[i].glucose - filteredData[i - 1].glucose) / (numInterpolatedPoints + 1);
 
-               for (let j = 1; j <= numInterpolatedPoints; j++) {
-                   const interpolatedTime = new Date(prevTime + j * 5 * 60 * 1000).toISOString(); // Add 5 minutes
-                   const interpolatedGlucose = filteredData[i - 1].glucose + glucoseDiff * j;
-                   filteredData.splice(i + (j - 1), 0, { glucose: interpolatedGlucose, dateString: interpolatedTime });
+const RSmaxDeltaBG = .2
+const currentGlucose = glucose[0].glucose;
+const prevGlucose1 = glucose[1].glucose;
+const prevGlucose2 = glucose[2].glucose;
+const prevGlucose3 = glucose[3].glucose;
+
+// BG Delta for current reading and prior reading
+const glucoseDiff_Now = prevGlucose1 - currentGlucose;
+const glucosedelta_Now = glucoseDiff_Now / prevGlucose1;
+const glucoseDiff_Prev = prevGlucose2 - prevGlucose1;
+const glucosedelta_Prev = glucoseDiff_Prev / prevGlucose2;
+
+const currentTime = new Date(glucose[0].datestring).getTime();
+const prevTime1 = new Date(glucose[1].datestring).getTime();
+const prevTime2 = new Date(glucose[2].datestring).getTime();
+const timeDiff_Now = (currentTime - prevTime1) / (1000 * 60); // Difference in minutes
+const timeDiff_Prev = (currentTime - prevTime2) / (1000 * 60); // Difference in minutes
+
+           if (timeDiff_Now > 10 || timeDiff_Prev > 15 || glucosedelta_Now >= RSmaxDeltaBG || glucosedelta_Prev >= RSmaxDeltaBG ) {
+               
+                      [Take Action]
+                      
                }
