b8bb1d9388eb92b0c5700b2ec5d6026acd118e69
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-02-03 13:38:29 -0500

    Update DisableSMBs.js
    Build code

--- DisableSMBs.js
+++ DisableSMBs.js
@@ -1,25 +1,24 @@
+// Sensor Safety: if data gaps or high BG delta, disable SMBs, UAMs, and smb_delivery_ratio_scaling
+         const RSmaxDeltaTick = 35 // single BG tick greater than x
+         const currentGlucose = glucose[0].glucose;
+         const prevGlucose1 = glucose[1].glucose;
+         const prevGlucose2 = glucose[2].glucose;
+         const prevGlucose3 = glucose[3].glucose;
 
+         // BG Difference for current reading and prior reading
+         const glucoseDiff_Now = prevGlucose1 - currentGlucose;
+         const glucoseDiff_Prev = prevGlucose2 - prevGlucose1;
 
-const RSmaxDeltaBG = .2
-const currentGlucose = glucose[0].glucose;
-const prevGlucose1 = glucose[1].glucose;
-const prevGlucose2 = glucose[2].glucose;
-const prevGlucose3 = glucose[3].glucose;
+         const currentTime = new Date(glucose[0].datestring).getTime();
+         const prevTime1 = new Date(glucose[1].datestring).getTime();
+         const prevTime2 = new Date(glucose[2].datestring).getTime();
+         const timeDiff_Now = (currentTime - prevTime1) / (1000 * 60); // Difference in minutes
+         const timeDiff_Prev = (currentTime - prevTime2) / (1000 * 60); // Difference in minutes
 
-// BG Delta for current reading and prior reading
-const glucoseDiff_Now = prevGlucose1 - currentGlucose;
-const glucosedelta_Now = glucoseDiff_Now / prevGlucose1;
-const glucoseDiff_Prev = prevGlucose2 - prevGlucose1;
-const glucosedelta_Prev = glucoseDiff_Prev / prevGlucose2;
-
-const currentTime = new Date(glucose[0].datestring).getTime();
-const prevTime1 = new Date(glucose[1].datestring).getTime();
-const prevTime2 = new Date(glucose[2].datestring).getTime();
-const timeDiff_Now = (currentTime - prevTime1) / (1000 * 60); // Difference in minutes
-const timeDiff_Prev = (currentTime - prevTime2) / (1000 * 60); // Difference in minutes
-
-           if (timeDiff_Now > 10 || timeDiff_Prev > 15 || glucosedelta_Now >= RSmaxDeltaBG || glucosedelta_Prev >= RSmaxDeltaBG ) {
+           if (timeDiff_Now >= 12 || timeDiff_Prev >= 17 || glucoseDiff_Now >= RSmaxDeltaTick || glucoseDiff_Prev >= RSmaxDeltaTick ) {
                
-                      [Take Action]
+                      profile.enableUAM = false;
+                      profile.enableSMB_always = false;
+                      enable_smb_delivery_ratio_scaling = false;    
                       
                }
