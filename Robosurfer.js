964f64980d59acc5eda0b6d3e7fe88ef9763b287
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-02-27 17:38:56 -0500

    Update Robosurfer.js
    Update Nightboost settings

--- Robosurfer.js
+++ Robosurfer.js
@@ -207,7 +207,7 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
          //Automation 1 Sigmoid - Threshold 1
          var Automation_1_minimumRatio_1 = .5;
          var Automation_1_maximumRatio_1 = 1.5;
-         var Automation_1_adjustmentFactor_1 = 1;
+         var Automation_1_adjustmentFactor_1 = .5;
 
          //Automation 1 Sigmoid - Threshold 2
          var Automation_1_minimumRatio_2 = .5;
