dc06673ca0eb94ee134bac505ed802c7f078a13c
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-02-29 14:41:23 -0500

    Update Mealboost.js
    Build Code

--- Mealboost.js
+++ Mealboost.js
@@ -16,13 +16,62 @@ function round(value, digits) {
          
 // The Mealboost Function
 
-  // Reduce hourly carb absorption to 5-minute carb absoorption
-     min_5m_carbabsorption = min_hourly_carb_absorption / (60 / 5);
+   //Increased Rate of Change (1.6mg/dl per minute)
+             if (glucoseRateOfChange_3Periods > 1.6) {
+             
+                //105-139 (Max: 2.1, AF 1)
+                if ((myGlucose >= Automation_1_BGThreshold_1 && myGlucose < Automation_1_BGThreshold_2)) {  
+                      // Set Nightboost Threshold 3 Factors    
+                     Automation_Status = Automation_1_name + " OnROCMax2.1";   
+                     NightBoost_Sigmoid_Min = Automation_1_minimumRatio_5;
+                     NightBoost_Sigmoid_Max = Automation_1_maximumRatio_5;
+                     NightBoost_Sigmoid_AF = Automation_1_adjustmentFactor_5;
+                     new_maxSMB = maxSMB + Automation_1_SMB_UAM_Minutes_Increase_HIGH;   
+                     new_maxUAM = maxUAM + Automation_1_SMB_UAM_Minutes_Increase_HIGH;
+                     profile.smb_delivery_ratio = Automation_1_SMB_DeliveryRatio_Increase_ACCEL;
+                }
+             
+                  // 140+ ((Max: 2.1, AF 1)
+                  if (myGlucose >= Automation_1_BGThreshold_2) {
+                     // Set Nightboost Threshold 3 Factors    
+                     Automation_Status = Automation_1_name + " OnROCMax2.1";
+                     NightBoost_Sigmoid_Min = Automation_1_minimumRatio_5;
+                     NightBoost_Sigmoid_Max = Automation_1_maximumRatio_5;
+                     NightBoost_Sigmoid_AF = Automation_1_adjustmentFactor_5;
+                     new_maxSMB = maxSMB + Automation_1_SMB_UAM_Minutes_Increase_HIGH;   
+                     new_maxUAM = maxUAM + Automation_1_SMB_UAM_Minutes_Increase_HIGH;
+                     profile.smb_delivery_ratio = Automation_1_SMB_DeliveryRatio_Increase_ACCEL;
+                  }
+             }
+                
+            //High Rate of Change (4mg/dl per minute)
+             if (glucoseRateOfChange_2Periods > 4 || glucoseRateOfChange_3Periods > 4) {  
 
-  // Calculate the dynamic min_5m_carbimpact
-   min_5m_carbimpact = (min_5m_carbabsorption * isf) / carb_factor;
-  
-  profile.min_5m_carbimpact = round(min_5m_carbimpact,2);
+                   //105-139 (Max: 2.25, AF 1)
+                  if ((myGlucose >= Automation_1_BGThreshold_1 && myGlucose < Automation_1_BGThreshold_2)) {  
+                        // Set Nightboost Threshold 4 Factors with Acceleration    
+                        Automation_Status = Automation_1_name + " OnHighROCMax2.25";
+                        NightBoost_Sigmoid_Min = Automation_1_minimumRatio_6;
+                        NightBoost_Sigmoid_Max = Automation_1_maximumRatio_6;
+                        NightBoost_Sigmoid_AF = Automation_1_adjustmentFactor_6;
+                        new_maxSMB = maxSMB + Automation_1_SMB_UAM_Minutes_Increase_ACCEL;   
+                        new_maxUAM = maxUAM + Automation_1_SMB_UAM_Minutes_Increase_ACCEL;
+                        profile.smb_delivery_ratio = Automation_1_SMB_DeliveryRatio_Increase_ACCEL;
+                }
+
+                   // 140+ ((Max: 2.25, AF 1)
+                  if (myGlucose >= Automation_1_BGThreshold_2) {
+                     // Set Nightboost Threshold 4 Factors with Acceleration    
+                     Automation_Status = Automation_1_name + " On HighROCMax2.25";
+                     NightBoost_Sigmoid_Min = Automation_1_minimumRatio_6;
+                     NightBoost_Sigmoid_Max = Automation_1_maximumRatio_6;
+                     NightBoost_Sigmoid_AF = Automation_1_adjustmentFactor_6;
+                     new_maxSMB = maxSMB + Automation_1_SMB_UAM_Minutes_Increase_ACCEL;   
+                     new_maxUAM = maxUAM + Automation_1_SMB_UAM_Minutes_Increase_ACCEL;
+                     profile.smb_delivery_ratio = Automation_1_SMB_DeliveryRatio_Increase_ACCEL;
+                  }
+                
+                }
  
 return "The min_5m_carbimpact has been adjusted to: " + round(profile.min_5m_carbimpact, 2) + ".";
 
