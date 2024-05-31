f69ec86a58c694dbad343f4175edb6d1615e9b40
macconnellk <69541225+macconnellk@users.noreply.github.com>
2024-05-30 21:48:28 -0400

    Update RoboSurfer_iAPSv4.js

--- RoboSurfer_iAPSv4.js
+++ RoboSurfer_iAPSv4.js
@@ -117,6 +117,7 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
    if (overridePercentage = .87) {
          enable_robosens = false;
          useOverride = false;
+         current_basal = current_basal / .87;
          dynamicVariables.useOverride = false;
          dynamicVariables.overridePercentage = 100;
          logOverride = "Robosens Off";
